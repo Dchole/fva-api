80822958026262b52d458bf7b56fe5aca44e4f84
Dchole <derekoware47@gmail.com>
2020-10-12 14:13:06 +0000

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '530058e80846215c36a8fbda9bbee4ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '78dca17d-febf-4e37-931d-f4c4c5986420'
+};
